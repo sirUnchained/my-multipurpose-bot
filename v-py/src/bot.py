aafaf5fd1b79ac14a9ae731cf28c453da6566801
sirunchained <bbehnam3831@gmail.com>
2025-09-25 21:56:52 +0330

    .

--- v-py/src/bot.py
+++ v-py/src/bot.py
@@ -1,6 +1,6 @@
 from telebot import TeleBot, apihelper
 from configs import BOT_TOKEN, PROXY
-from bot_modles import start
+from bot_modules import start
 
 apihelper.proxy = {"https": PROXY}
 bot: TeleBot = TeleBot(BOT_TOKEN, parse_mode=None)
